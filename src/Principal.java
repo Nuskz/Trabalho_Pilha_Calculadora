18361117b1e83859ef76366309058135d2f8532b
Nuskz <arielliasjr200@gmail.com>
2023-03-25 14:42:22 -0300

    interface concluida

--- src/Principal.java
+++ src/Principal.java
@@ -1,25 +1,42 @@
-import java.io.InputStream;
+
 import java.util.Scanner;
 
 public class Principal {
-    public Principal(){
+    public Principal() {
         Scanner s = new Scanner(System.in);
         Calculadora c;
         int opc;
-        while(opc != 3){
-            System.out.println("Escolha qual pilha usar:\n(1)Pilha Vetor\n(2)Pilha Encadeada");
-            opc = s.nextInt();
-            System.out.println("Agora digite a expressão(pos fixa):");
-            String expressao = s.nextLine();
-            s.close();
-            if(opc == 1){
+        String expressao = "";
+        System.out.println("Escolha qual pilha usar:\n(1)Pilha Vetor\n(2)Pilha Encadeada\n(3)sair");
+        opc = s.nextInt();
+        while (opc != 3) {
+            expressao = s.nextLine();
+            if (opc == 1) {
+                System.out.println("Agora digite a expressão(pos fixa):");
+                expressao = s.nextLine();
                 c = new Calculadora(expressao, true);
-            } else if (opc == 2){
+                System.out.println("O resultado da expressão[" + expressao + "] é: " + c.getResposta());
+            } else if (opc == 2) {
+                System.out.println("Agora digite a expressão(pos fixa):");
+                expressao = s.nextLine();
                 c = new Calculadora(expressao, false);
+                System.out.println("O resultado da expressão[" + expressao + "] é: " + c.getResposta());
+            } else if (opc == 3) {
+                break;
             } else {
                 System.out.println("Opção inválida");
+
             }
+
+            System.out.println("Escolha qual pilha usar:\n(1)Pilha Vetor\n(2)Pilha Encadeada\n(3)sair");
+            opc = s.nextInt();
+
         }
-        
+
+        s.close();
+    }
+
+    public static void main(String[] args) {
+        Principal p = new Principal();
     }
 }
